f6699989c3f1ba25b931d1b052a0d1fdad1f3769
gabrieldigon <gabrieldiasgonca@gmail.com>
2022-11-29 08:56:58 -0400

    IncludingListOfEvents

---
+++ TwekPresentation/Views/ListOfEventsView.swift
@@ -0,0 +1,84 @@
+//
+//
+//  ListEventsView.swift
+//  PresentationsApp
+//
+//  Created by Alex A. Rocha on 16/11/22.
+//
+
+import SwiftUI
+
+struct ListOfEventsView: View {
+    
+    var presentation: Presentation
+    
+    @State private var showingSheet = false
+    
+    var body: some View {
+        ZStack{
+            Color(.DarkFundoIphone).edgesIgnoringSafeArea(.all)
+            VStack (spacing: 16) {
+                Text(presentation.descript)
+
+                    .font(.system(size: 17))
+                    .multilineTextAlignment(.leading)
+                HStack {
+                    Image(systemName: "applewatch.radiowaves.left.and.right")
+                    Text("Haptics on Apple Watch " + (presentation.haptics ? "enabled" : "disabled"))
+                        .font(.system(size: 15))
+                    
+                }
+                HStack {
+                    Text("\(secondsToMinutesSecondsWithText(presentation.totalTime))")
+                        .font(.system(size: 15))
+                    Image(systemName: "circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 4, height: 4)
+                        .foregroundColor(.gray)
+                    Text("\(presentation.events.count) events")
+                        .font(.system(size: 15))
+                }
+                HStack (spacing: 5){
+                    Image(systemName: "info.circle")
+                    Text("Tap in the timeline to add an event.")
+                        .font(.system(size: 17))
+                }
+                .foregroundColor(.gray)
+                .padding(.horizontal, 7)
+                .padding(.vertical, 7)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.gray, lineWidth: 1)
+                )
+                List(presentation.events) { event in
+                    EventCardView(event: event)
+//                        .listRowBackground(Color(.CorFundoCard))
+                        .listRowSeparator(.hidden)
+                }
+                .sheet(isPresented: $showingSheet, content: {
+                    CreateEventModal(presentation: presentation)
+                })
+            }
+            .padding(.top,15)
+           
+            .navigationTitle(Text(presentation.title))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button(action: {
+                    showingSheet.toggle()
+                }, label: {
+                    Image(systemName: "plus")
+                        .foregroundColor(Color(.RoxoWatch))
+                })
+            }
+            
+        }
+    }
+    
+    struct ListOfEventsView_Previews: PreviewProvider {
+        static var previews: some View {
+            ListOfEventsView(presentation: Presentation())
+        }
+    }
+}
