15d543a9f80b72a1aa02cbd471f0740db8673e17
Alex A. Rocha <lex99r@gmail.com>
2022-11-30 09:28:18 -0400

    Fixing back button alert

--- TwekPresentation Watch App/WatchViews/EventCard.swift
+++ TwekPresentation Watch App/WatchViews/EventCard.swift
@@ -6,6 +6,8 @@ struct EventCard: View {
     @State var evento: SimpleEvents
     @State var start = Date()
     
+    @State var isHapticsPresentation: Bool
+    
     @State var progress = 0.0
     let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
@@ -13,11 +15,11 @@ struct EventCard: View {
         ZStack{
             Color(.CorFundoCard)
                 .cornerRadius(6)
-            VStack(spacing: 0){
+            VStack(spacing: 0) {
                 Color(.CorPadraoCard)
                     .cornerRadius(6)
                     .padding()
-                HStack{
+                HStack {
                     ProgressView(value: progress, total: evento.eventDuration)
                         .tint(Color(.RoxoWatch))
                         .onReceive(timer) { _ in
@@ -25,11 +27,13 @@ struct EventCard: View {
                                 progress += 0.1
                             } else if progress >= evento.eventDuration {
                                 timer.upstream.connect().cancel()
-                                WKInterfaceDevice.current().play(.stop)
+                                if isHapticsPresentation {
+                                    WKInterfaceDevice.current().play(.stop)
+                                }
                             }
                         }
                 }
-                HStack{
+                HStack {
                     Text(evento.eventDescription)
                         .scenePadding()
                 }
@@ -44,14 +48,12 @@ struct EventCard: View {
 struct EventCardShow: View {
     var apresentacao : SimplePresentation
     var body: some View {
-        if apresentacao.events.count == 0{
+        if apresentacao.events.count == 0 {
             
-        }
-        else{
-            TabView  {
-                //construir card aq
-                ForEach((0...apresentacao.events.count-1), id: \.self) {
-                    EventCard(evento:apresentacao.events[$0])
+        } else {
+            TabView {
+                ForEach((0...apresentacao.events.count - 1), id: \.self) {
+                    EventCard(evento: apresentacao.events[$0], isHapticsPresentation: apresentacao.haptics)
                 }
             }
             .tabViewStyle(.carousel)
